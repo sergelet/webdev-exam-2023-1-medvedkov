7ba373559312529af62b134419723b15c4ccb15c
sergelet <medvedkov_sn@mail.ru>
2024-01-09 20:24:30 +0300

    Главная страница

--- main.js
+++ main.js
@@ -1,67 +1,106 @@
 'use strict';
+const url = new URL("http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes?api_key=26a75d98-76bf-48a8-9acd-e75e991c56cb");
+const pang = {
+   page: 1,
+   obj_on_page: 9,
+   counter: 0,
+};
 
-const BASE_URL = new URL("http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes?api_key=b972478d-9fb3-4651-9efc-4e96ffc84c43");
-
-/*const xhr = new XMLHttpRequest()
-xhr.open('GET', BASE_URL)
-
-xhr.responseType = 'json'
-
-xhr.onload = () => {
-    console.log(xhr.response)
-}
-
-xhr.send()*/
-// Функция для получения данных
 function getData() {
-    var xhr = new XMLHttpRequest();
+   var xhr = new XMLHttpRequest();
    
-    // Запрос GET к серверу
-    xhr.open('GET', BASE_URL, true);
+   // Запрос GET к серверу
+   xhr.open('GET', url, true);
    
-    // Обработка ответа от сервера
-    xhr.onload = function() {
-       if (xhr.status === 200) {
+   // Обработка ответа от сервера
+   xhr.onload = function() {
+      if (xhr.status === 200) {
          // Парсинг JSON-объекта
          var data = JSON.parse(xhr.responseText);
-   
+
          // Вывод информации на страницу
          displayData(data);
-       } else {
+      } else {
          console.error('Ошибка:', xhr.status);
-       }
-    };
+      }
+   };
    
-    // Обработка ошибок
-    xhr.onerror = function() {
-       console.error('Ошибка при запросе данных');
-    };
+   // Обработка ошибок
+   xhr.onerror = function() {
+      console.error('Ошибка при запросе данных');
+   };
    
-    // Отправка запроса
-    xhr.send();
+   // Отправка запроса
+   xhr.send();
+}
+   
+// Функция для вывода информации на страницу
+function displayData(data) {
+   var table = document.getElementById('path');
+   var tbody = table.getElementsByTagName('tbody')[0];
+   
+   // Очистка таблицы от старых данных
+   while (tbody.firstChild) {
+      tbody.removeChild(tbody.firstChild);
    }
    
-   // Функция для вывода информации на страницу
-   function displayData(data) {
-    var table = '<table><tr><th>Название</th><th>Описание</th><th>Основной объект</th></tr><tr></tr>';
-   
-    // Цикл по каждому элементу данных
-    for (var i = 0; i < data.length; i++) {
-      table += '<tr>';
-      //table += '<td>' + data[i].id + '</td>';
-      table += '<td>' + data[i].name + '</td>';
-      table += '<td>' + data[i].description + '</td>';
-      //table += '<td>' + data[i].created_at + '</td>';
-      table += '<td>' + data[i].mainObject + '</td>';
-      table += '<td>' + '<button name = "select"> Выбрать </button>'  + '</td>'
-      table += '</tr>';
+   clear(tbody); //Очистка
+
+   pang.counter = Math.ceil(data.length / pang.obj_on_page);
+   var start = (pang.page - 1) * pang.obj_on_page;
+   var end = start + pang.obj_on_page;
+   var displayedData = data.slice(start, end);
+
+   // Цикл по каждому элементу данных
+   for (var i = 0; i < displayedData.length; i++) {
+      var obj = displayedData[i];
+      var row = tbody.insertRow();
+
+      var elem1 = row.insertCell();
+      elem1.innerHTML = obj.name;
+      
+      var elem2 = row.insertCell();
+      elem2.innerHTML = obj.description;
+      
+      var elem3 = row.insertCell();
+      var elem4 = row.insertCell();
+      
+      var mainObject = obj.mainObject;
+      var mainObjectCell = document.createElement('ul');
+      var list = document.createElement('list');
+      list.textContent = mainObject;
+      mainObjectCell.appendChild(list);      
+      elem3.appendChild(mainObjectCell);
+      
+      var btn = document.createElement('button');
+      btn.textContent = 'Выбрать';
+      elem4.appendChild(btn);
+   }
+      
+}
+
+//Функции для пагинации
+
+function clear(body){
+   body.innerHTML = '';
+}
+
+function getBackPage() {
+    if (pang.page > 1) {
+       pang.page--;
+       getData();
     }
-   
-    table += '</table>';
-   
-    // Вставка таблицы на страницу
-    document.getElementById('table-container').innerHTML = table;
+ }
+ 
+
+function getNextPage() {
+   if (pang.page < pang.counter) {
+      pang.page++;
+      getData();
    }
-   
-   // Вызов функции для получения данных
+}
+
+// Вызов функции для получения данных
+window.onload = () => {
    getData();
+};
